1b6714b54401d2f91c0378415f6e3fb598d87fc4
Hsingai Altaica <draltaica@yahoo.com>
2024-12-08 07:02:35 -0800

    fixed a bug in Mate Resourse allocation equation

--- src/mate_resource_allocation/mod.rs
+++ src/mate_resource_allocation/mod.rs
@@ -3,7 +3,11 @@ use std::collections::HashMap;
 use crate::Number;
 
 type AgentId = usize;
-pub fn compute_investment(values: &HashMap<AgentId, Number>, recieved: &HashMap<AgentId, Number>, given: &HashMap<AgentId, Number>) -> HashMap<AgentId, Number>{
+/// in the ideal situation the portial og what we give a mate will be propotinal to what we invested
+/// so we compare the actual share to the black slate to account for the change of the mates value
+/// this also add response of not investing in investing in someone who we aren't effective in investing in
+/// and also makes us more responsive to the reletive ammount of attention given us as if they are giving more that the other then the share will get a boost which then couses us to invest more into them whou should couse us to give them more
+pub fn compute_investment(sensitivity: Number, values: &HashMap<AgentId, Number>, recieved: &HashMap<AgentId, Number>, given: &HashMap<AgentId, Number>) -> HashMap<AgentId, Number>{
     let total_value: Number = values.iter().map(|(_, x)|x).sum();
 
     let total_given: Number = given.iter().map(|(_, x)|x).sum();
@@ -12,7 +16,8 @@ pub fn compute_investment(values: &HashMap<AgentId, Number>, recieved: &HashMap<
         let first = value / total_value;
         let this_given = given.get(id).cloned().unwrap_or(0.into());
         let second = this_given / total_given;
-        let mult = first / second;
+        let mult = second / first;
+        let mult = ((mult * sensitivity) + (1.0-sensitivity)) / 2.0;
         let this_recieved = recieved.get(id).cloned().unwrap_or(0.into());
         (*id, this_given * this_recieved * mult)
 
@@ -28,4 +33,43 @@ pub fn compute_investment(values: &HashMap<AgentId, Number>, recieved: &HashMap<
     
     let investment = mate_give * mate_recieve * mult;
 */
+}
+#[cfg(test)]
+mod tests {
+    use std::collections::BTreeMap;
+
+    use super::*;
+
+    #[test]
+    fn feedback(){
+        let values = HashMap::from([
+            (0, 1.0),
+            (1, 1.0),
+            (2, 1.0),
+        ]);
+        let recieved = HashMap::from([
+            (0, 1.0),
+            (1, 2.0),
+            (2, 1.0),
+        ]);
+        let given = HashMap::from([
+            (0, 1.0),
+            (1, 1.0),
+            (2, 1.0),
+        ]);
+        let given2 = compute_investment(0.50, &values, &recieved, &given);
+        let recieved2 = HashMap::from([
+            (0, 1.0),
+            (1, 1.0),
+            (2, 1.0),
+        ]);
+        let given3 = compute_investment(0.50, &values, &recieved2, &given2);
+        let given4 = compute_investment(0.50, &values, &recieved2, &given3);
+        println!("1:{:?}", given.iter().collect::<BTreeMap<&AgentId, &Number>>());
+
+        println!("2:{:?}", given2.iter().collect::<BTreeMap<&AgentId, &Number>>());
+        println!("3:{:?}", given3.iter().collect::<BTreeMap<&AgentId, &Number>>());
+        println!("4:{:?}", given4.iter().collect::<BTreeMap<&AgentId, &Number>>());
+        assert_eq!(given3, HashMap::new());
+    }
 }
