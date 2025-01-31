f5d9cc5eaa438824356d1c49d068433aaa84bd34
Hsingai Altaica <draltaica@yahoo.com>
2025-01-30 23:26:35 -0800

    added zero_to_one_infinity_to_zero module to social_sim, it's for computing first inpressions

---
+++ src/social_sim/zero_to_one_infinity_to_zero.rs
@@ -0,0 +1,14 @@
+/// this is for calculating first inpressions
+/// you calc some `diffrance_in_personalities`
+/// then `let first_impression = how_well_their_interaction_when * zero2one_infinity_to_zero_throught_point(diffrance_in_personalities, 0.01, max_diffrance_in_personalities);`
+
+pub fn calc_zero2one_infinity_to_zero_coefficient(target_input: Number, target_output: Number) -> Number {
+    (Number::ONE - target_output) / (target_output * target_input)
+}
+pub fn zero2one_infinity_to_zero_with_coefficient(i: Number, coefficient: Number) -> Number {
+    1/((i * coefficient) + Number::ONE)
+}
+pub fn zero2one_infinity_to_zero_throught_point(i: Number, target_input: Number, target_output: Number) -> Number {
+    let coefficient = calc_zero2one_infinity_to_zero_coefficient(target_output, target_input);
+    zero2one_infinity_to_zero_with_coefficient(i, coefficient)
+}
