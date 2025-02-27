1d9bdbf94b753d9de67acca7c24c912a3db1350f
Hsingai Altaica <draltaica@yahoo.com>
2025-02-26 18:16:17 -0800

    updated danja_distance

--- src/util/danja_distance.rs
+++ src/util/danja_distance.rs
@@ -4,11 +4,20 @@ type Number =f64;
 /// acturaly it's an upper bound on the distance
 /// the greate the diffreance between the to the less te percant error
 pub fn danja_distance(x:Number, y:Number) -> Number {
-    let (max,min) = if y < x {(x,y)} else {(y,x)};
-    let d = max - min;
-    let q = max + (min * (1.0 / max));
-    let l = 0.5 * (min * 0.020671430 * d);
-    (( q + max + min) / 2.0 ) - l
+    let (max, min) = if y < x {(x,y)} else {(y,x)};
+    let diff = max - min;
+    let under = max + (min * (1.0 / max));
+
+    // these where fitted to 
+//          (1,1),        (10,10),      (100,100),        (1000,1),    (1000, 100),    (1000, 1000),      (1001, 1),     (1100, 100) the error is:
+// 4.99450053e-01  4.94500531e-01  4.45005306e-01  -1.77946164e-02  1.97436080e-04  -4.99469412e-02  1.77768173e-02  -1.77692237e-04
+    
+    let [min_diff_c, min_diff_m, max_diff_c, max_diff_m] = [-5.03056589e-02, -4.04488225e-04, -3.60307257e-02, 3.60033238e-05];
+
+    let min_correction = min * (min_diff_c + (diff * min_diff_m));
+    let max_correction = max * (max_diff_c + (diff * max_diff_m));
+    let correction = min_correction + max_correction;
+    (( under + max + min) / 2.0 ) + correction
 }
 /*
 fn main(){
