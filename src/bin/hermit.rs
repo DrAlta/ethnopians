fa92c7ebf47b0458f52e061040a531dedb97c0ba
Hsingai Altaica <draltaica@yahoo.com>
2025-03-23 12:56:09 -0700

    made unit test for main hermitAI task

--- src/bin/hermit.rs
+++ src/bin/hermit.rs
@@ -1,3 +1,4 @@
+/*
 use std::collections::HashMap;
 
 use ethnolib::sandbox::{
@@ -8,92 +9,9 @@ use ethnolib::sandbox::{
     EntityId, Item,
 };
 
-/*
 enum Prayer{
     FindInInventory { item_class},
 
 }
 */
-fn main() {
-    plant_vegs_test()
-}
-fn plant_vegs_test() {
-    // Set up the world
-    let my_self = EntityId::from_raw(0);
-    let house = EntityId::from_raw(5);
-    let garden_1 = EntityId::from_raw(51);
-    let garden_2 = EntityId::from_raw(52);
-    let garden_3 = EntityId::from_raw(52);
-    let garden_4 = EntityId::from_raw(54);
-    let item_types: HashMap<bevy::ecs::entity::Entity, Item> = HashMap::from([
-        (my_self, Item::Agent),
-        (house, Item::House),
-        (garden_1, Item::Stone),
-        (garden_2, Item::Stone),
-        (garden_3, Item::Stone),
-        (garden_4, Item::Stone),
-    ]);
-
-    let mut blackboard: Blackboard<String, BlackboardValue> = Blackboard::new();
-    blackboard.insert(
-        "self".to_owned(),
-        Variable::Chit(BlackboardValue::EntityId(my_self)),
-    );
-    blackboard.insert(
-        // I should change the GetIsInventoryGE to hold a BlackBoardValue instadt of a key
-        "knife".to_owned(),
-        Variable::Chit(BlackboardValue::String("Knife".to_owned())),
-    );
-    blackboard.insert(
-        // I should change the GetIsInventoryGE to hold a BlackBoardValue instadt of a key
-        "stone".to_owned(),
-        Variable::Chit(BlackboardValue::String("Stone".to_owned())),
-    );
-    blackboard.insert(
-        // I should change the GetIsInventoryGE to hold a BlackBoardValue instadt of a key
-        "garden_location".to_owned(),
-        Variable::Chit(BlackboardValue::Coord { x: 6, y: 9 }),
-    );
-
-    // set up  the dummy values
-    let find_in_inventory = vec![EntityId::from_raw(70), EntityId::from_raw(71)];
-    let find_nearest = vec![]; //house];
-    let get_entities = vec![/*[
-        (StackItem::Int(0), StackItem::EntityId(garden_1)),
-        (StackItem::Int(1), StackItem::EntityId(garden_2)),
-        (StackItem::Int(2), StackItem::EntityId(garden_3)),
-        (StackItem::Int(3), StackItem::EntityId(garden_4)),
-    ]
-    .try_into()
-    .unwrap()
-    */];
-    let get_energy = vec![]; //4];
-
-    let get_location = vec![]; //(6, 9)];
-    let get_hp = vec![]; //4];
-    let get_is_inventory_ge = vec![false, true, true]; // true];
-    let running = vec![
-        true, true, true, true, true, true, true, true, true, true, true, true, true, true, true,
-        true, true, true, true, true, true, true, true, true, true, true, true, true, true, true,
-        true, true, true, true, true, true, true, true, true, true, true,
-    ];
-    // done setting up dummy values
-
-    let task_db = get_hermit_behavior_task();
-
-    task_testing_harness(
-        "plant_vegs",
-        task_db,
-        vec![StackItem::success()],
-        find_in_inventory,
-        find_nearest,
-        get_entities,
-        get_energy,
-        get_location,
-        get_hp,
-        get_is_inventory_ge,
-        running,
-        blackboard,
-        item_types,
-    )
-}
+fn main() {}
--- src/sandbox/ai/hermit/tests/hermit_ai_run_test.rs
+++ src/sandbox/ai/hermit/tests/hermit_ai_run_test.rs
@@ -1,62 +1,165 @@
-use qol::logy;
+use std::collections::HashMap;
 
 use crate::sandbox::{
-    ai::{get_hermit_behavior_task, Blackboard, BlackboardValue, StackItem, Status, Variable, CPU},
-    EntityId,
+    ai::{
+        get_hermit_behavior_task, task_testing_harness, Blackboard, BlackboardValue, StackItem,
+        Variable,
+    },
+    EntityId, Item,
 };
 
 #[test]
-fn hermit_ai_run_test() {
-    let mut blackboard = Blackboard::new();
+fn hermit_test() {
+    // Set up the world
+    let my_self = EntityId::from_raw(0);
+    let house = EntityId::from_raw(5);
+    let garden_1 = EntityId::from_raw(51);
+    let garden_2 = EntityId::from_raw(52);
+    let garden_3 = EntityId::from_raw(52);
+    let garden_4 = EntityId::from_raw(54);
+    let item_types: HashMap<bevy::ecs::entity::Entity, Item> = HashMap::from([
+        (my_self, Item::Agent),
+        (house, Item::House),
+        (garden_1, Item::Stone),
+        (garden_2, Item::Stone),
+        (garden_3, Item::Stone),
+        (garden_4, Item::Stone),
+    ]);
+
+    let mut blackboard: Blackboard<String, BlackboardValue> = Blackboard::new();
     blackboard.insert(
         "self".to_owned(),
-        Variable::Chit(BlackboardValue::EntityId(EntityId::from_raw(0))),
+        Variable::Chit(BlackboardValue::EntityId(my_self)),
     );
     blackboard.insert(
+        // I should change the GetIsInventoryGE to hold a BlackBoardValue instadt of a key
         "food".to_owned(),
         Variable::Chit(BlackboardValue::String("Veggie".to_owned())),
     );
+    blackboard.insert(
+        // I should change the GetIsInventoryGE to hold a BlackBoardValue instadt of a key
+        "knife".to_owned(),
+        Variable::Chit(BlackboardValue::String("Knife".to_owned())),
+    );
+    blackboard.insert(
+        // I should change the GetIsInventoryGE to hold a BlackBoardValue instadt of a key
+        "stone".to_owned(),
+        Variable::Chit(BlackboardValue::String("Stone".to_owned())),
+    );
+
+    // set up  the dummy values
+    let find_in_inventory = vec![
+        EntityId::from_raw(70),
+        EntityId::from_raw(71),
+        EntityId::from_raw(72),
+        EntityId::from_raw(73),
+        EntityId::from_raw(74),
+        EntityId::from_raw(75),
+        EntityId::from_raw(76),
+        EntityId::from_raw(77),
+        EntityId::from_raw(78),
+        EntityId::from_raw(79),
+        EntityId::from_raw(80),
+        EntityId::from_raw(81),
+        EntityId::from_raw(82),
+        EntityId::from_raw(83),
+        EntityId::from_raw(84),
+        EntityId::from_raw(85),
+        EntityId::from_raw(86),
+        EntityId::from_raw(87),
+        EntityId::from_raw(88),
+        EntityId::from_raw(89),
+    ];
+    let find_nearest = vec![house, house, house];
+    let get_entities = vec![
+        [
+            (StackItem::Int(0), StackItem::EntityId(garden_1)),
+            (StackItem::Int(1), StackItem::EntityId(garden_2)),
+            (StackItem::Int(2), StackItem::EntityId(garden_3)),
+            (StackItem::Int(3), StackItem::EntityId(garden_4)),
+        ]
+        .try_into()
+        .unwrap(),
+        [
+            (StackItem::Int(0), StackItem::EntityId(garden_1)),
+            (StackItem::Int(1), StackItem::EntityId(garden_2)),
+            (StackItem::Int(2), StackItem::EntityId(garden_3)),
+            (StackItem::Int(3), StackItem::EntityId(garden_4)),
+        ]
+        .try_into()
+        .unwrap(),
+        [
+            (StackItem::Int(0), StackItem::EntityId(garden_1)),
+            (StackItem::Int(1), StackItem::EntityId(garden_2)),
+            (StackItem::Int(2), StackItem::EntityId(garden_3)),
+            (StackItem::Int(3), StackItem::EntityId(garden_4)),
+        ]
+        .try_into()
+        .unwrap(),
+        [
+            (StackItem::Int(0), StackItem::EntityId(garden_1)),
+            (StackItem::Int(1), StackItem::EntityId(garden_2)),
+            (StackItem::Int(2), StackItem::EntityId(garden_3)),
+            (StackItem::Int(3), StackItem::EntityId(garden_4)),
+        ]
+        .try_into()
+        .unwrap(),
+        [
+            (StackItem::Int(0), StackItem::EntityId(garden_1)),
+            (StackItem::Int(1), StackItem::EntityId(garden_2)),
+            (StackItem::Int(2), StackItem::EntityId(garden_3)),
+            (StackItem::Int(3), StackItem::EntityId(garden_4)),
+        ]
+        .try_into()
+        .unwrap(),
+        [
+            (StackItem::Int(0), StackItem::EntityId(garden_1)),
+            (StackItem::Int(1), StackItem::EntityId(garden_2)),
+            (StackItem::Int(2), StackItem::EntityId(garden_3)),
+            (StackItem::Int(3), StackItem::EntityId(garden_4)),
+        ]
+        .try_into()
+        .unwrap(),
+    ];
+    let get_energy = vec![4];
+
+    let get_location = vec![
+        (6, 1),
+        (6, 2),
+        (6, 3),
+        (6, 4),
+        (6, 5),
+        (6, 6),
+        (6, 7),
+        (6, 8),
+        (6, 9),
+        (6, 10),
+    ];
+    let get_hp = vec![4];
+    let get_is_inventory_ge = vec![false, true, true]; // true];
+    let running = vec![
+        true, true, true, true, true, true, true, true, true, true, true, true, true, true, true,
+        true, true, true, true, true, true, true, true, true, true, true, true, true, true, true,
+        true, true, true, true, true, true, true, true, true, true, true, true, true, true, true,
+        true, true, true, true, true, true,
+    ];
+    // done setting up dummy values
 
     let task_db = get_hermit_behavior_task();
-    let mut cpu = CPU::load("hermit".to_owned());
-    loop {
-        logy!(
-            "trace-tests",
-            "\nexecuting {:?}\nstack: {:?}\n",
-            cpu.pc,
-            cpu.stack
-        );
-        match cpu.step(&task_db, &mut blackboard) {
-            Ok(status) => match status {
-                Status::UseOn(_a, _b) =>{
-                    todo!()
-                }
-                Status::FindInInventory { item_class: _ } =>{
-                    todo!()
-                }
-                Status::Success => {
-                    logy!("trace-tests", "hermit ai succeeded\n{cpu:?}");
-                    break
-                },
-                Status::Failure => todo!(),
-                Status::FindNearest { ../*x, y, item_class*/ } => todo!(),
-                Status::GetEnergy(_entity) => {
-                    logy!("trace-tests", "giving dummy value for GetEnergy on {_entity}");
-                    cpu.stack.push(StackItem::some(StackItem::Int(5)));
-                },
-                Status::GetLocation(_entity) => todo!(),
-                Status::GetHp(_entity) => todo!(),
-                Status::GetIsInventoryGE { agent: _agent, item_class: _item_class, amount: _amount } => {
-                    logy!("trace-tests", "giving dummy value for if {_agent} has GE {_amount} of {_item_class:?}");
-                    cpu.stack.push(StackItem::success());
-                },
-                Status::GetEntities { ../*min_x, min_y, max_x, max_y*/ } => todo!(),
-                Status::RemoveEntitiesOfType(_item) => todo!(),
-                Status::RetainEntitiesOfType(_item) => todo!(),
-                Status::Running(_inpulse_id) => todo!(),
-                Status::None => (),
-            },
-            Err(err) => panic!("hermitAI gave error: {err}"),
-        }
-    }
+
+    task_testing_harness(
+        "hermit",
+        task_db,
+        vec![StackItem::success()],
+        find_in_inventory,
+        find_nearest,
+        get_entities,
+        get_energy,
+        get_location,
+        get_hp,
+        get_is_inventory_ge,
+        running,
+        blackboard,
+        item_types,
+    )
 }
