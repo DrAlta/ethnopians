57c3de055d036cd026b1526d8b9d03d70bf06909
Hsingai Altaica <draltaica@yahoo.com>
2025-05-22 12:39:45 -0700

    added From<V> for Vabiabble

--- src/sandbox/ai/cpu/blackboard/variable.rs
+++ src/sandbox/ai/cpu/blackboard/variable.rs
@@ -5,3 +5,13 @@ pub enum Variable<K: Debug, V: Debug> {
     Defer(K),
     Chit(V),
 }
+impl<K: Debug, V: Debug> Variable<K, V> {
+    pub fn defer(value: K) -> Self {
+        Self::Defer(value)
+    }
+}
+impl<K: Debug, V: Debug> From<V> for Variable<K, V> {
+    fn from(value: V) -> Self {
+        Self::Chit(value)
+    }
+}
