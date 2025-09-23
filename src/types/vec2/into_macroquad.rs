daa994071b4bde03c7f23750b656a3379ee9d084
Hsingai Altaica <draltaica@yahoo.com>
2025-09-23 08:15:46 -0700

    fixed macroquad freature

--- src/types/vec2/into_macroquad.rs
+++ src/types/vec2/into_macroquad.rs
@@ -3,8 +3,8 @@ use crate::Number;
 impl Into<macroquad::math::Vec2> for crate::Vec2 {
     fn into(self) -> macroquad::math::Vec2 {
         macroquad::math::Vec2 {
-            x: self.x as f32,
-            y: self.y as f32,
+            x: self.x.into(),
+            y: self.y.into(),
         }
     }
 }
@@ -12,8 +12,8 @@ impl Into<macroquad::math::Vec2> for crate::Vec2 {
 impl From<macroquad::math::Vec2> for crate::Vec2 {
     fn from(value: macroquad::math::Vec2) -> Self {
         Self {
-            x: value.x as Number,
-            y: value.y as Number,
+            x: value.x.into(),
+            y: value.y.into(),
         }
     }
 }
