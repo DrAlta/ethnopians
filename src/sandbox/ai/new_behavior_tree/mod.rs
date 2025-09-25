5eab5e8de872ac820eaf1a55206db3a311a08696
Hsingai Altaica <draltaica@yahoo.com>
2025-09-24 20:34:47 -0700

    added notes on how the new behavoir tree works

--- src/sandbox/ai/new_behavior_tree/mod.rs
+++ src/sandbox/ai/new_behavior_tree/mod.rs
@@ -1,15 +1,18 @@
-use std::collections::{BTreeMap, BTreeSet};
+/*
+parent.Tick(none)-> call(parentstate1, child, None)
+child.tick(none) -> Wait(childstate)
+parent.uptick(parentstate1, Wait(childstate)) ->wait(parentstate2{childstate}) 
+parent.Tick(parentstate2{childstate}) -> call(parentstate3, child, childstate)
+child.tick(childstate) -> Success
+parent.Uptick(parentstate3, Success)
+*/
 
-use qol::logy;
-
-use crate::sandbox::ai::{Blackboard, BlackboardKey, BlackboardValue};
 
 mod prayer;
 pub use prayer::Prayer;
-mod status;
-pub use status::Status;
-
 mod state;
 pub use state::State;
+mod status;
+pub use status::Status;
 mod node;
 pub use node::Node;
